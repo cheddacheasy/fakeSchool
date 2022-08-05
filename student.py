97f915a4f243916dc0341d3bbbc4be461bb48e2d
Ellipses <cheddacheezy.00@gmail.com>
2022-08-05 00:18:10 -0700

    Added docstrings to student.py class module

--- student.py
+++ student.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field 
+from dataclasses import dataclass, field
 import randomFile
 from faker import Faker
 fake = Faker()
@@ -6,6 +6,27 @@ fake = Faker()
 # add repr method maybe or leave it for person_info
 @dataclass
 class Person:
+    """
+    A class to represent administrative based student object
+
+
+    Attributes
+    ----------
+    person_full_name : str
+    first_name : str
+    last_name : str
+    student_id : int
+    student_phone_number : int
+    email : str
+    student_ssid : str
+    student_address : str
+
+    Methods
+    -------
+    person_info(self) -> None
+        Prints out all attributes of the person object
+    """
+
     person_full_name: str = field(init=False)
     first_name: str = field(init=False)
     last_name: str = field(init=False)
@@ -16,17 +37,30 @@ class Person:
     student_address: str = field(init=False)
 
     def __post_init__(self):
+        """
+        Constructs all the necessary attributes for the person object
+        """
+
         self.person_full_name = randomFile.full_name()
         self.first_name = self.person_full_name.split()[0]
         self.last_name = self.person_full_name.rsplit(None, 1)[-1]
         self.email = randomFile.stud_email(self.person_full_name)
-        self.student_id= randomFile.student_id_number()
+        self.student_id = randomFile.student_id_number()
         self.student_phone_number = fake.phone_number()
         self.student_ssid = fake.ssn()
         self.student_address = fake.address()
 
-
     def person_info(self) -> None:
+        """ 
+        display the administrative information of the student-person object
+        generated
+
+                parameters:
+                    self: instance of student person object created
+
+                return:
+                    none
+        """
         print("First Name:", self.first_name)
         print("Last Name:", self.last_name)
         print("Email:", self.email)
@@ -38,17 +72,46 @@ class Person:
 
 @dataclass
 class Student(Person): 
+    """
+    A class to represen the acadmeic based student object
+
+
+    Attributes
+    ----------
+    college : str
+    degree_type : str
+    year_status : str
+    college_hour_status : str
+    current_credit_hours : int
+    total_credit_hours : int
+    student_gpa_current : float
+    graduation_student_status : bool
+    student_full_name : str
+
+    Methods
+    -------
+    person_info(self) -> None
+        Prints out all attributes of the person object
+
+    college_info(self) -> None
+        Prints out all attributes of the student object
+    """
+
     college: str = field(init=False)
     degree_type: str = field(init=False)
     year_status: str = field(init=False)
     college_hour_status: str = field(init=False)
     current_credit_hours: int = field(init=False)
-    total_credit_hours: int = field(init=False) 
-    student_gpa_current: float  = field(init=False)
-    graduation_student_status: bool = field(init=False) 
+    total_credit_hours: int = field(init=False)
+    student_gpa_current: float = field(init=False)
+    graduation_student_status: bool = field(init=False)
     student_full_name: str = field(init=False)
 
     def __post_init__(self):
+        """
+        Constructs all the necessary attributes for the student object
+        """
+
         Person.__post_init__(self)
         email2: str
         self.degree_type = randomFile.student_Degree()
@@ -66,8 +129,17 @@ class Student(Person):
         self.total_credit_hours = randomFile.credits_completed(self.degree_type)
         self.graduation_student_status = randomFile.graduation_status(self.total_credit_hours, self.degree_type)
 
-
     def college_info(self) -> str:
+         """
+        display the academic information of the student object
+        generated
+
+                parameters:
+                    self: instance of student object created
+
+                return:
+                    none
+        """
         print("Institution: ", self.college)
         print("Degree: ", self.degree_type)
         print("Current GPA: ", self.student_gpa_current)
