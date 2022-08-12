d287516ef0197bfdd2952f309b0fec1cbced3ed9
Ellipses <cheddacheezy.00@gmail.com>
2022-08-12 01:23:52 -0700

    Added documentation to functions.py. Look at functions.py for further instruction

--- functions.py
+++ functions.py
@@ -15,18 +15,18 @@ import os
 
 
 # sex tells the function if you want a male, female, or no sex
-def randFullName(sex: int)->str:
+def randFullName(sex: int) -> str:
     """
-    Returns a randomly generated first and last name based on sex.
+    returns a randomly generated first and last name based on sex.
 
-            Parameters:
+            parameters:
                 sex (int) : integer that represents the sex of the name
                     0 = male
                     1 = female
                     any other integer will produce a random male or femal full name
 
-            Return:
-                name (str) : String of first name that represent a male or female sex
+            return:
+                name (str) : string of first and last name that represent a male or female sex
     """
 
     if sex == 0:
@@ -36,6 +36,7 @@ def randFullName(sex: int)->str:
     else:
         name = names.get_full_name()
     return name
+
 
 def randFullNameMain() -> None:
     """
@@ -58,53 +59,145 @@ def randFullNameMain() -> None:
 
 
 def randFirstName(sex: int)->str:
+    """
+    returns a randomly generated first name based on sex.
+
+            parameters:
+                sex (int) : integer that represents the sex of the name
+                    0 = male
+                    1 = female
+                    any other integer will produce a random male or femal full name
+
+            return:
+                name (str) : string of first name that represent a male or female sex
+    """
+
     if sex == 0:
-        name = names.get_first_name()
+        name = names.get_first_name('male')
     elif sex == 1:
-        name = names.get_first_name(gender='male')
-    else:
         name = names.get_first_name(gender='female')
+    else:
+        name = names.get_first_name()
     return name
 
-def randFirstNameMain() -> None:
-    print("How many first names would you like to generate?")
-    numNames = int(input(">"))
+
+def randFirstNameMain() -> list[str]:
+    """
+    returns a randomly generated list of first names based on sex.
+
+            parameters:
+                sex (int) : integer that represents the sex of the name
+                    0 = male
+                    1 = female
+                    any other integer will produce a random male or femal full name
+
+            return:
+                name (str) : string of first name that represent a male or female sex
+    """
+
     namesList = []
-    i = 0
-    while i < numNames:
-        randSex = random.randint(0, 2)
-        testName = randFirstName(randSex)
-        i += 1
+
+    print("How many first names would you like to generate?")
+    numNames = int(input("> "))
+    print("Choose from the following optios: ")
+    print("Press 0                 All Male")
+    print("Press 1                 All Female")
+    print("Press 2                 All Random")
+    nameType = int(input("> "))
+
+    if nameType == 0:
+        i = 0
+        while i < numNames:
+            testName = randFirstName(0)
+            namesList.append(testName)
+            i += 1
+    elif nameType == 0:
+        i = 0
+        while i < numNames:
+            testName = randFirstName(1)
+            namesList.append(testName)
+            i += 1
+    elif nameType == 0:
+        i = 0
+        while i < numNames:
+            randSex = random.randint(0, 2)
+            namesList.append(testName)
+            testName = randFirstName(randSex)
+            i += 1
+
+    return namesList
 
 
+def randLastName() -> str:
+    """
+    returns a randomly generate last name based on sex.
 
-def randLastName()->str:
+            parameters:
+                None
+            return:
+                name (str) : string of randomly generated last name
+    """
+
     name = names.get_last_name()
     return name
 
+
 def randLastNameMain() -> None:
-    print("How many first names would you like to generate?")
+    """
+    returns a randomly generate list of last names based on sex.
+
+            parameters:
+                None
+            return:
+                name (str) : List of randomly generated last names
+    """
+    lastNames = []
+    print("How many last names would you like to generate?")
     numNames = int(input(">"))
     i = 0
     while i < numNames:
-        randSex = random.randint(0, 2)
-        testName = randLastName(randSex)
+        testName = randLastName()
+        lastNames.append(testName)
         i += 1
 
 
+def infoMenu() -> None:
+    """
+    Displays a list of options for the user can choose from for randomly generating
+    names.
+    The user can choose to randomlu generate first names, last names, full names,
+    or mix first, last, and full names
 
-def infoMenu():
+            parameters:
+                None
+            return:
+                None
+    """
+
     print("Press 1: If you would like to get only first names")
     print("Press 2: If you would like to get only last names")
     print("Press 3: If you would like to get only full names")
     print("Press 4: If you would like to get a mix of first, last, and full names")
     print("Press 0: Exit")
 
-def infochoice():
+
+def infochoice() -> int:
+    """
+    Receives the user input choice from the user in regards to their randomziation choice
+
+            parameters:
+                None
+            return:
+                UserChoice (int) : represents the user choice for name randomization 
+    """
+
     UserChoice = int(input("> "))
     return UserChoice
 
-
+# left off here
+# finish documentation
+# write out error handling 
+# write out test for this file
 def menuSwitchBoard(userChoice: int):
     if type(userChoice) != int or userChoice < 0 or userChoice > 5:
         raise ValueError("Invalid Choice")
